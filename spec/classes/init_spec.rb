668257d3fecd91adcbffae7843c030c5069936c5
Jordan Conway <jconway@linuxfoundation.org>
2015-12-07 15:59:55 -0500

    added travis.yml and using rspec-puppet-facts for tests
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,19 +1,24 @@
 require 'spec_helper'
 describe 'logstash' do
-  let(:facts) {
-    {
-      :fqdn            => 'test.example.com',
-      :hostname        => 'test',
-      :ipaddress       => '192.168.0.1',
-      :operatingsystem => 'CentOS',
-      :operatingsystemmajrelease => '7',
-      :osfamily        => 'RedHat'
-    }
-  }
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('logstash') }
-    it { should contain_class('logstash::forwarder') }
+#  let(:facts) {
+#    {
+#      :fqdn            => 'test.example.com',
+#      :hostname        => 'test',
+#      :ipaddress       => '192.168.0.1',
+#      :operatingsystem => 'CentOS',
+#      :operatingsystemmajrelease => '7',
+#      :osfamily        => 'RedHat'
+#    }
+#  }
+ on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+      context 'with defaults for all parameters' do
+        it { should contain_class('logstash') }
+        it { should contain_class('logstash::forwarder') }
+      end
+    end
   end
-
 end
