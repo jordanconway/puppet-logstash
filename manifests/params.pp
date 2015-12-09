211cb2c6cdf133f910da98be8c1074b8cf29bc88
Jordan Conway <jconway@linuxfoundation.org>
2015-12-09 15:59:11 -0500

    Made timeout an int rather than string
    Signed-off-by: Jordan Conway <jconway@linuxfoundation.org>

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@ class logstash::params {
   $package = 'https://download.elastic.co/logstash-forwarder/binaries/logstash-forwarder-0.4.0-1.x86_64.rpm'
   $servers = ["logstash.${::domain}:${port}"]
   $port = 5000
-  $timeout = '15'
+  $timeout = 15
   $use_ssl = false
   $ssl_ca = '/etc/pki/tls/certs/logstash-forwarder.crt'
   $paths = { 'paths' => ['/var/log/messages', '/var/log/secure'],
